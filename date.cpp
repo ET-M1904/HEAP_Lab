b762460be0eff4fbe0d9d6eb04db849f11ced8a4
ET-M1904 <erik.taylor@bsu.edu>
2025-02-21 15:54:32 +0000

    Progress made

---
+++ date.cpp
@@ -0,0 +1,39 @@
+#include "date.h"
+#include <iostream>
+
+Date::Date(){
+	month = "";
+	day = "";
+	year = "";
+}
+
+void Date::init(std::string dateString){
+	std::stringsteam ss(dateString)
+
+	Date::month = tMonth;
+	Date::day = tDay;
+	Date::year = tYear;
+
+	std::string tMonth;
+	std::string tDay;
+	std::string tYear;
+
+	ss.clear();
+	ss.str("");
+
+	ss << tDay << " " << tMonth << " " << tYear;
+	ss >> day >> month >> year;
+}
+
+void Date::printDate(){
+	std::string months[] = { "Null", "January", "Febuary",
+	       			"March", "April", "May", "June",
+			       	"July", "August", "September",
+				"October", "November", "December"};
+	std::cout << months[month] << " ";
+	std::cout << day << ", " << year << std::endl;
+}
+
+
+
+
